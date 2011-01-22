cad01826886e106c1cf31454551e8fa7d428d41e
karagodin <ekaragodin@gmail.com>
2011-01-22 15:14:54 +0700

    Added loads parents configs.

--- FConfig.php
+++ FConfig.php
@@ -11,20 +11,56 @@ class FConfig extends CBehavior {
     public $configs = array();
     public $currentConfig = '';
 
+    protected $chainConfigs = array();
+
+    /**
+     * Load configs from files and replace current configuration.
+     * @return CApplication
+     */
     public function loadConfigure() {
         $app = $this->getOwner();
 
         if (isset($this->configs[$this->currentConfig])) {
-            $config = require(Yii::getPathOfAlias($this->configDir . '.' . $this->currentConfig) . '.php');
+            $config = $this->loadChain($this->currentConfig);
 
-            if (is_array($config)) {
-                foreach ($config as $key => $value)
-                    $app->$key = $value;
-            }
-
+            foreach ($config as $key => $value)
+                $app->$key = $value;
         }
 
         return $app;
+    }
+
+    /**
+     * Recursively loads the configuration files.
+     * @param  $name
+     * @return array
+     */
+    protected function loadChain($name) {
+        if (isset($this->chainConfigs[$name])) {
+            throw new CException('Config "' . $name . '" already load!');
+        }
+
+        $this->chainConfigs[$name] = $this->loadFromFile($name);
+        $result = $this->chainConfigs[$name];
+
+        if (isset($this->configs[$name]['parent'])) {
+            $parentConfig = $this->loadChain($this->configs[$name]['parent']);
+            $result = CMap::mergeArray($result, $parentConfig);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $name file configuration.
+     * @return array
+     */
+    protected function loadFromFile($name) {
+        $config = require(Yii::getPathOfAlias($this->configDir . '.' . $name) . '.php');
+        if (is_array($config))
+            return $config;
+        else
+            return array();
     }
 
 }
